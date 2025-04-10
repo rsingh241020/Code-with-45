97fa8709c5305f0503735f6a043190616b3df554
Rohit Kumar <126482082+rsingh241020@users.noreply.github.com>
2025-04-10 11:09:26 +0530

    Discuss about the Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"Use strict"; // treat all js code as newwer Version
+
+// alert(3+3) // We are Using Node Js Not Browser
+console.log(3
+    +3) // Code readability should be high 
+console.log("Hii Rohit ")
+// MDN Documentation  for js
+let name = "Rohit"
+let age = "18"
+let isLoggedIn = false
+
+//Data Types of JS
+// number => 2 to power 53
+// bigint 
+// String => ""
+//boolean = true /false
+//null => standalone value
+//undefined => 
+// symbol => unique
+
+
+//object
+console.log(typeof "Rohit") // String 
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
