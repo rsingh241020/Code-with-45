be6d1896a6a46eed0096ca805587f9e757a91f35
Rohit Kumar <126482082+rsingh241020@users.noreply.github.com>
2025-04-20 00:43:54 +0530

    Time and Date

---
+++ 01_basics/07_date.js
@@ -0,0 +1,20 @@
+// Dates
+// let myDate = new Date(); // Update Date
+// console.log(myDate.toString()); // Change in String
+// console.log(myDate.toDateString()); //date and time
+// console.log(myDate.toLocaleDateString()); // local time
+// console.log(typeof myDate); // Object type
+
+//let myCreateDate = new Date(2025,3,20); //date and time
+//let myCreateDate = new Date(2025,3,20,00,0,0);
+//let myCreateDate = new Date("20-04-2025");
+let myCreateDate = new Date("2025-04-20");
+console.log(myCreateDate);
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDate);
+let today = new Date();
+console.log(`Today the Date and Day is ${today.toDateString()}`);
+
