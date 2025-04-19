65195f89f4a9d01cd1fb330c00ce443527057f06
Rohit Kumar <126482082+rsingh241020@users.noreply.github.com>
2025-04-19 13:18:39 +0530

    Discuss On The Topic of operations in JavaScript

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,67 @@
+let x = "45Hitman"
+let value= Number(x)
+//console.log(typeof value);
+//console.log(value)
+// a => string             b => number
+//     45Hitman                 NaN
+
+// c => NaN                d => 45Hitman
+//     number                   string
+
+let score = "45"
+//console.log(typeof(score)); // String
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber); // Number
+
+let score1 = "45Hitman"
+//console.log(typeof(score1)); // String
+let valueInNumber1 = Number(score1) //
+//console.log(typeof valueInNumber1); // Number 
+//console.log(valueInNumber1); // NaN
+
+// "33 " => 33
+//"33abc" => NaN Not a Number
+// true => 1 false =0
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+ // 1=> true 0 =>false
+ // "" =>false
+ //"rohit" => true
+
+ let sumNumber = 33
+ let stringNumber = String(sumNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+// ***************Operations*****************
+let v = 3
+let negValue = -v
+// console.log(negValue) // -3
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2-2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+
+let str1 = "hello"
+let str2 = "Rohit"
+
+let str3 = str1+str2 //helloRohit
+
+// console.log(str3);
+
+// console.log("1"+"2");//12
+// console.log(1+"2");//12
+// console.log("1"+2);//12
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32
+console.log(3+4*5%3);//
+
+
+
+
+
+
